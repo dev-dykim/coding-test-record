0237f9cc5950396e8ac934a05c9fd2c5084ef9cb
doyeonKim <96133075+dev-dykim@users.noreply.github.com>
2023-01-21 01:02:33 +0900

    [level 0] Title: 옹알이 (1), Time: 0.14 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -1,21 +1,15 @@
 class Solution {
     public int solution(String[] babbling) {
-        String words[] = {"aya", "ye", "woo", "ma"};
-        int cnt = 0;
-
-        for (String target : babbling) {
-            for (int i = 0; i < words.length; i++) {
-                if (words[i].equals(target)) {
-                    cnt++;
-                    break;
-                } else {
-                    if (target.length() >= words[i].length() && words[i].equals(target.substring(0, words[i].length()))) {
-                        target = target.substring(words[i].length());
-                        i = -1;
-                    }
-                }
-            }
+        int answer = 0;
+        for (String b : babbling) {
+            b = b.replace("aya", "1");
+            b = b.replace("ye", "1");
+            b = b.replace("woo", "1");
+            b = b.replace("ma", "1");
+            b = b.replace("1", "");
+            if (b.isEmpty())
+                answer++;
         }
-        return cnt;
+        return answer;
     }
 }
