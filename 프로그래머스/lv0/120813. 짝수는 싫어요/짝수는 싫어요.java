315174550004b4eeb80ac09161b56466ac997e3b
doyeonKim <96133075+dev-dykim@users.noreply.github.com>
2023-01-25 09:37:17 +0900

    [level 0] Title: 짝수는 싫어요, Time: 1.99 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -1,10 +1,14 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 class Solution {
     public int[] solution(int n) {
-        int[] answer = n % 2 == 0 ? new int[n / 2] : new int[n / 2 + 1];
-        for (int i = 1, idx = 0; i <= n; i++) {
-            if (i % 2 != 0)  // 홀수일때만
-                answer[idx++] = i;  // answer에 넣는다.
-        }
-        return answer;
+        List<Integer> list = new ArrayList<>();  // List를 사용하면 배열 크기 정하지 않아도 된다.
+        for (int i = 1; i <= n; i++)
+            if (i % 2 != 0)
+                list.add(i);
+
+        return list.stream().mapToInt(Integer::intValue).toArray();  // 대신, list를 int 배열로 바꾸는게 조금 까다로워진다.
     }
 }
