669020c17d6dc0a1585da2828b0689b6df5a31e4
doyeonKim <96133075+dev-dykim@users.noreply.github.com>
2023-01-26 10:30:48 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -1,11 +1,11 @@
 class Solution {
     public int solution(int price) {
         if (price >= 100000 && price < 300000)
-            return (int) (price - (price * 0.05));
+            return (int) (price * 0.95);
         else if (price >= 300000 && price < 500000)
-            return (int) (price - (price * 0.1));
+            return (int) (price * 0.9);
         else if (price >= 500000)
-            return (int) (price - (price * 0.2));
+            return (int) (price * 0.8);
         else
             return price;
     }
