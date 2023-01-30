486d99f4233f9bec4f47ab81dc79eb897c2f7feb
doyeonKim <96133075+dev-dykim@users.noreply.github.com>
2023-01-30 18:43:04 +0900

    [level 0] Title: 369게임, Time: 1.21 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.java
+++ 프로그래머스/lv0/120891. 369게임/369게임.java
@@ -2,6 +2,6 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(int order) {
-        return (int) Arrays.stream(String.valueOf(order).split("")).filter(s -> s.matches("[3|6|9]")).count();
+        return (int) Arrays.stream(String.valueOf(order).split("")).filter(s -> s.matches("[369]")).count();
     }
 }
