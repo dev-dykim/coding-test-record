1deb83d340526ea4ef3f8ff8f3c5b81fc2667b80
doyeonKim <96133075+dev-dykim@users.noreply.github.com>
2023-01-31 17:25:27 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.63 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
+++ 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -1,10 +1,15 @@
 class Keypad {
-    int num;
+    String num;
     int row;
     int col;
 
-    public Keypad(int num, int row, int col) {
+    public Keypad(String num, int row, int col) {
         this.num = num;
+        this.row = row;
+        this.col = col;
+    }
+
+    public void setRowCol(int row, int col) {
         this.row = row;
         this.col = col;
     }
@@ -14,48 +19,42 @@ class Solution {
     public String solution(int[] numbers, String hand) {
         StringBuilder sb = new StringBuilder();
         Keypad[] keypad = new Keypad[10];
+        Keypad left = new Keypad("L", 3, 0);
+        Keypad right = new Keypad("R", 3, 2);
 
         // Keypad 클래스에 번호(num), 번호판의 row, col 정보 넣기
-        keypad[0] = new Keypad(0, 3, 1);
+        keypad[0] = new Keypad("0", 3, 1);
         for (int idx = 1; idx < 10; idx++) {
             for (int r = 0; r < 3; r++) {
                 for (int c = 0; c < 3; c++) {
-                    keypad[idx] = new Keypad(idx++, r, c);
+                    keypad[idx] = new Keypad(String.valueOf(idx++), r, c);
                 }
             }
         }
 
-        int[] L = {3, 0}; // {L의 row, L의 col} 처음 위치
-        int[] R = {3, 2}; // {R의 row, R의 col} 처음 위치
-
         for (int n : numbers) {
+            String next;
             if (n == 1 | n == 4 | n == 7) {
-                sb.append("L");
-                L[0] = n / 3;
-                L[1] = 0;
+                next = left.num;
+                left.setRowCol(n/3, 0);
             } else if (n == 3 | n == 6 | n == 9) {
-                sb.append("R");
-                R[0] = n / 3 - 1;
-                R[1] = 2;
+                next = right.num;
+                right.setRowCol(n/3 - 1, 2);
             } else {
-                int L_dist = Math.abs(keypad[n].row - L[0]) + Math.abs(keypad[n].col - L[1]);
-                int R_dist = Math.abs(keypad[n].row - R[0]) + Math.abs(keypad[n].col - R[1]);
-                String next;
-                if (L_dist == R_dist) {
-                    next = hand.equals("right") ? "R" : "L";
-                }
-                else {
-                    next = L_dist < R_dist ? "L" : "R";
-                }
-                if (next.equals("R")) {
-                    sb.append("R");
-                    R[0] = keypad[n].row;
-                    R[1] = keypad[n].col;
-                } else {
-                    sb.append("L");
-                    L[0] = keypad[n].row;
-                    L[1] = keypad[n].col;
-                }
+                int L_dist = Math.abs(keypad[n].row - left.row) + Math.abs(keypad[n].col - left.col);
+                int R_dist = Math.abs(keypad[n].row - right.row) + Math.abs(keypad[n].col - right.col);
+
+                if (L_dist == R_dist)
+                    next = hand.equals("right") ? right.num : left.num;
+                else
+                    next = L_dist < R_dist ? left.num : right.num;
+            }
+            if (next.equals(right.num)) {
+                sb.append(right.num);
+                right.setRowCol(keypad[n].row, keypad[n].col);
+            } else {
+                sb.append(left.num);
+                left.setRowCol(keypad[n].row, keypad[n].col);
             }
         }
 
