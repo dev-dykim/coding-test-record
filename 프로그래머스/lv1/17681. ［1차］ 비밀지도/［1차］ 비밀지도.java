9279d73fdf95f97cb0403e34e95e288a14ab5699
doyeonKim <96133075+dev-dykim@users.noreply.github.com>
2023-01-31 09:44:02 +0900

    [level 1] Title: [1차] 비밀지도, Time: 9.19 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class Solution {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            String format = "%0" + n + "d";
+            String binary = Integer.toBinaryString(arr1[i] | arr2[i]);
+            answer[i] = String.format(format, Long.parseLong(binary));
+            answer[i] = answer[i].replaceAll("1", "#");
+            answer[i] = answer[i].replaceAll("0", " ");
+        }
+
+        return answer;
+    }
+}
