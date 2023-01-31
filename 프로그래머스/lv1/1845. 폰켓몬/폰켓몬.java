be4e8326b064eef70423b5f1d48834637f3eddac
doyeonKim <96133075+dev-dykim@users.noreply.github.com>
2023-01-31 09:15:33 +0900

    [level 1] Title: 폰켓몬, Time: 2.16 ms, Memory: 87.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int solution(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int n : nums) {
+            set.add(n);
+        }
+
+        return Math.min(set.size(), nums.length / 2); 
+    }
+}
