a2d55ea0beaffb453865508ba490bd46377d3e89
doyeonKim <96133075+dev-dykim@users.noreply.github.com>
2023-04-19 16:01:43 +0900

    [Silver III] Title: N과 M (2), Time: 124 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,85 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static int N, M;
+    static int[] selected;
+
+    static void input() {
+        N = scan.nextInt();
+        M = scan.nextInt();
+        selected = new int[M];
+    }
+
+    static void rec_func(int k, int start) {
+        if (k == M) {
+            for (int x : selected) {
+                sb.append(x).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int cand = start; cand <= N; cand++) {
+            selected[k] = cand;
+            rec_func(k+1, cand + 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        input();
+        rec_func(0, 1);
+        System.out.println(sb.toString());
+    }
+
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
+
