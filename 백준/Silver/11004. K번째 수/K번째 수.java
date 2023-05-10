5f2731ba089bd52a0bb9d0d5f1a06c969e72e528
doyeonKim <96133075+dev-dykim@users.noreply.github.com>
2023-05-10 16:44:38 +0900

    [Silver V] Title: K번째 수, Time: 1140 ms, Memory: 368376 KB -BaekjoonHub

--- 백준/Silver/11004. K번째 수/K번째 수.java
+++ 백준/Silver/11004. K번째 수/K번째 수.java
@@ -19,8 +19,55 @@ public class Main {
     }
 
     static void pro() {
-        Arrays.sort(A);
+//        Arrays.sort(A);
+        quickSort(0, N-1);
+
         System.out.println(A[K-1]);
+    }
+
+    static void quickSort(int start, int end) {
+        if (start < end) {
+            int pivot = partition(start, end);
+            if (pivot == K-1)
+                return;
+            else if (pivot > K-1)
+                quickSort(start, pivot - 1);
+            else
+                quickSort(pivot + 1, end);
+        }
+    }
+
+    static int partition(int start, int end) {
+
+        if (start + 1 == end) {
+            if (A[start] > A[end])
+                swap(start, end);
+            return end;
+        }
+
+        int mid = (start + end) / 2;
+        swap(start, mid);
+        int pivot = A[start];
+        int i = start + 1, j = end;
+        while (i <= j) {
+            while (pivot > A[i] && i < N-1) i++;
+            while (pivot < A[j] && j > 0) j--;
+            if (i <= j) {
+                swap(i, j);
+                i++;
+                j--;
+            }
+        }
+
+        A[start] = A[j];
+        A[j] = pivot;
+        return j;
+    }
+
+    static void swap(int x, int y) {
+        int temp = A[x];
+        A[x] = A[y];
+        A[y] = temp;
     }
 
     public static void main(String[] args) {
