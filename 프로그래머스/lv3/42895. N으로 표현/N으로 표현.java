70120204ddd4c0d1b696962da3db05231b5674eb
doyeonKim <96133075+dev-dykim@users.noreply.github.com>
2023-05-20 22:15:59 +0900

    [level 3] Title: N으로 표현, Time: 23.56 ms, Memory: 84.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42895. N으로 표현/N으로 표현.java
@@ -0,0 +1,32 @@
+class Solution {
+    static int N, number, answer = Integer.MAX_VALUE;
+    static int[] arr;
+    static boolean found = true;
+
+    public int solution(int N, int number) {
+        this.N = N;
+        this.number = number;
+
+        arr = new int[6];
+        for (int i = 1; i < arr.length; i++) {
+            arr[i] = arr[i-1] * 10 + N;
+        }
+
+        rec_func(0, 0);
+
+        return answer > 8 ? -1 : answer;
+    }
+
+    private void rec_func(int result, int cnt) {
+        if (cnt > 8 || result == number) {
+            answer = Math.min(answer, cnt);
+        } else {
+            for (int i = 1; i < arr.length; i++) {
+                rec_func(result + arr[i], cnt + i);
+                rec_func(result - arr[i], cnt + i);
+                rec_func(result * arr[i], cnt + i);
+                rec_func(result / arr[i], cnt + i);
+            }
+        }
+    }
+}
